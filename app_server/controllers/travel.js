733a2c47ac7cdfee4fdfd602ad19c1ba2eb8d394
Jacob Virgilio <jacob.virgilio@snhu.edu>
2025-05-24 15:51:56 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,11 @@
-/* GET Homepage */
-const index = (req, res) => {
-    res.render('index', { title: "Travlr Getaways"});
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
+/* GET travel view */
+const travel = (req, res) => {
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 
-module.exports ={
-    travel
-    
-}
+module.exports = {
+  travel,
+};
